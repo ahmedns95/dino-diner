197a899df0f4feb2eef8013a8f6de16d1d147f32
Ahmed Alnassar <ahmedns95@users.campus>
2019-09-13 16:38:06 -0500

    Transferred files from other project

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entree
+{
+    public class DinoNuggets
+    {
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+             get
+             {
+                  List<string> ingredients = new List<string>() { "6 chicken nuggets" };
+                  //if (bun) ingredients.Add("Whole Wheat Bun");
+                  return ingredients;
+                }
+            }
+
+        public DinoNuggets()
+        {
+             this.Price = 4.25;
+             this.Calories = 59;
+        }
+
+        public void AddAdditionalNugget()
+        {
+
+        }
+
+
+
+    }
+}
